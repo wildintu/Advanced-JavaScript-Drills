65334c373e3c91161e988e5eadfd1f3c73ea7c0e
wildintu <hello@wildintu.com>
2020-02-03 13:30:41 -0600

    first fruit

--- script.js
+++ script.js
@@ -10,11 +10,20 @@
 //     var name = 'lexi';
 //     console.log(name);
 // }
-console.log('some text');
-let avg = findAvg(2,2);
-console.log('some text', avg);
-function findAvg(a,b) {
-    console.log('some text');
-    var answer = (a+b)/2;
-    return answer;
+// console.log('some text');
+// let avg = findAvg(2,2);
+// console.log('some text', avg);
+// function findAvg(a,b) {
+//     console.log('some text');
+//     var answer = (a+b)/2;
+//     return answer;
+// }
+
+fruits = ['bananas', 'apples', 'oranges']
+let favFruit;
+
+pickFruit();
+
+function pickFruit() {
+    console.log(fruits[0]);
 }
